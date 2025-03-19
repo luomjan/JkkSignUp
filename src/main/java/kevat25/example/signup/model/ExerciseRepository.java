c3827679b082d556a0fa60ae1d17cf8f1a6f3f6f
Nerilka <jamansik@hotmail.com>
2025-03-19 15:38:15 +0200

    lisätty restControlleri

--- src/main/java/kevat25/example/signup/model/ExerciseRepository.java
+++ src/main/java/kevat25/example/signup/model/ExerciseRepository.java
@@ -1,7 +1,9 @@
 package kevat25.example.signup.model;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface ExerciseRepository extends CrudRepository<Exercise, Long> {
-    
+    List<Exercise> findByGenreGenre(String genre);
 }
